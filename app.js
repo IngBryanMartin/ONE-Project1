6b1821590c9370bc3ba30b7d21d1e0de13bd903d
Bryan <ing.bryanmartin@gmail.com>
2024-01-24 16:23:41 -0500

    Completed code

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+//adivina el numero secreto, el cual es 6
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
-
+let numeroUsuario = prompt("Me indicas un número por favor: ");
 console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+if(numeroUsuario == numeroSecreto){
+    console.log('Acertaste el número')
+} else {
+    console.log('incorrecto')
 }
+
+
+//adivina mi nombre, el cual es Bryan
+
+// let nombreUsuario = prompt('Ingresa el nombre que creas correcto');
+// let nombreCorrecto = "Bryan";
+
+// if (nombreUsuario == nombreCorrecto){
+//     alert ('¡Enhorabuena! El nombre es ' + nombreCorrecto + '!');
+// }
